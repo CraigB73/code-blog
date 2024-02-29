c8732cc62d46eef5ca5fd2e8d34cb6a3ebcf9a13
CraigB73 <craigbritton73@gmail.com>
2024-02-29 21:03:37 +0100

    Add post model

--- code_blog/settings.py
+++ code_blog/settings.py
@@ -129,3 +129,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.herokuapp.com",
+    "http://127.0.0.1:8000"
+]
