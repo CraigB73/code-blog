f66b4f7be6b101a4bd27d4d474ebc4a005f4278c
CraigB73 <craigbritton73@gmail.com>
2024-03-04 17:28:31 +0100

    Test to fix issue when deploy in Heroku

--- code_blog/settings.py
+++ code_blog/settings.py
@@ -27,9 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5mw6iv)&g%srwx&b-@62##8-n1_qvc)x!y0&^35vj2nag(4&8l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG =False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*",
+                 ".herokuapp.com",]
 
 
 # Application definition
